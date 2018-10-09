5baa08819d7970f5445c040deeb5ffb5127fabf0
TiPiKej <romukkemot@gmail.com>
2018-10-09 16:01:25 +0200

    Updates

--- js/index.js
+++ js/index.js
@@ -1,20 +1,21 @@
 (() => {
+  const pre = document.documentURI;
 
-
-$LAB
-	.script('/js/core.min.js')
-		.wait(() => {
-			funkcje.meta_adds();
-		})
-	.script('/js/dom_elements.min.js')
-		.wait(() => {
-			dom.nav();
-			dom.pages();
-			dom.jezyki();
-			funkcje.loader();
-		})
-	.script('/cw/host.js')
-		.wait(() => {
-			if(document.querySelector('.backButton')) document.querySelector('.backButton').remove();
-		})
+  $LAB
+    .script(pre + "/js/core.min.js")
+    .wait(() => {
+      funkcje.meta_adds();
+    })
+    .script(pre + "/js/dom_elements.min.js")
+    .wait(() => {
+      dom.nav();
+      dom.pages();
+      dom.jezyki();
+      funkcje.loader();
+    })
+    .script(pre + "/cw/host.js")
+    .wait(() => {
+      if (document.querySelector(".backButton"))
+        document.querySelector(".backButton").remove();
+    });
 })();
