8f03c422adc848879e46cb3c74589b32804f5afc
TiPiKej <romukkemot@gmail.com>
2018-10-09 16:29:23 +0200

    1.2.3.1

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,20 +1,21 @@
 (() => {
+  const pre = document.URL.indexOf("github") !== -1 ? "/Portfolio" : "";
 
-
-$LAB
-	.script('/js/core.min.js')
-		.wait(() => {
-			funkcje.meta_adds();
-		})
-	.script('/js/dom_elements.min.js')
-		.wait(() => {
-			dom.nav();
-			dom.pages();
-			dom.jezyki();
-			funkcje.loader();
-		})
-	.script('/cw/host.js')
-		.wait(() => {
-			if(document.querySelector('.backButton')) document.querySelector('.backButton').remove();
-		})
+  $LAB
+    .script(pre + "/js/core.min.js")
+    .wait(() => {
+      funkcje.meta_adds();
+    })
+    .script(pre + "/js/dom_elements.min.js")
+    .wait(() => {
+      dom.nav();
+      dom.pages();
+      dom.jezyki();
+      funkcje.loader();
+    })
+    .script(pre + "/cw/host.js")
+    .wait(() => {
+      if (document.querySelector(".backButton"))
+        document.querySelector(".backButton").remove();
+    });
 })();
