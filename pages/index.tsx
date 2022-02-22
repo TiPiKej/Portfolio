1414fb8aaf9e2c5ea515f2322d986d0d957ba373
Tomek <romukkemot@gmail.com>
2022-02-22 19:01:48 +0100

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,33 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
+interface NavigationStruct {
+  content: string,
+  href: string,
+}
 
 export default function Home() {
+  const navigationData: NavigationStruct[] = [
+    {
+      content: "O mnie",
+      href: "/o-mnie"
+    },
+    {
+      content: "Znane technologie",
+      href: "/znane-technologie"
+    },
+  ]
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
-        </a>
-      </footer>
+    <div className="grid grid-cols-1 lg:grid-cols-2 bg-zinc-900 lg:bg-gradient-to-r lg:from-zinc-900 lg:via-zinc-900 lg:to-red-900 min-h-screen">
+      <div className="flex flex-col flex-nowrap text-sky-300 text-3xl sm:text-6xl 2xl:text-7xl space-y-8 items-center justify-center">
+        {navigationData.map(n => (
+          <Link href={n.href}>
+            <a>{n.content}</a>
+          </Link>
+        ))}
+      </div>
+      {/* <div className="bg-origin-content p-10 bg-center bg-no-repeat bg-[url('https://picsum.photos/700/700')] brightness-50 rounded-full hidden lg:block"></div> */}
     </div>
   )
 }
